aeb12300d52abbdeb58589eb69bbafd463621c3d
Trinity Sazo <tsazo@andover.edu>
2018-05-21 16:52:30 -0400

    Add opinion.php
    Add links to other pages in nav.html. Add opinion.php to put up written articles.

---
+++ opinion.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+
+		<link href="https://fonts.googleapis.com/css?family=Monsieur+La+Doulaise|Work+Sans" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="styles.css">
+
+	</head>
+	<body style="margin: 0px 0px 0px 0px; background-color: #FAFAFA; font-family: 'Work Sans', sans-serif;">
+		<div id="body" style="">
+			<div id="header" style="padding: 10%; padding-top: 5%;padding-bottom: 20px;">
+				<div><a style="font-size: 90px;" href="#home">Opinion</a><hr></hr></div>
+			</div>
+
+			<?php include 'nav.html' ?>
+			
+			<div id="mid" style="text-align: center;">
+				<div class="imgs"><img style="width: 100%;" id="slider" src="IMG_1173.JPG"></div>
+				<div class="imgs" style="text-align: left; margin: 10%; padding: 3%; background-color: #FAF4D4;">
+					<p>Lorem ipsum dolor amet pitchfork occupy gluten-free, pour-over jean shorts shoreditch blue bottle echo park. Vaporware retro wayfarers, copper mug health goth tofu mixtape. Umami hell of humblebrag, jianbing raw denim tumblr you probably haven't heard of them meh ennui post-ironic jean shorts heirloom. Snackwave copper mug slow-carb adaptogen, tacos actually messenger bag pork belly craft beer sriracha food truck microdosing helvetica twee. Skateboard irony chambray prism live-edge pabst selvage post-ironic fam. Plaid scenester brunch single-origin coffee.</p>
+					<p>Roof party VHS tumeric trust fund, dreamcatcher shoreditch coloring book pok pok meh chicharrones leggings ethical etsy 3 wolf moon. Leggings synth af paleo 8-bit sustainable man braid bicycle rights meggings. Tote bag vegan yr put a bird on it four dollar toast lo-fi blue bottle wayfarers glossier trust fund small batch microdosing raclette edison bulb succulents. Humblebrag salvia blog seitan, pour-over raclette wolf paleo.</p>
+					<p>Adaptogen portland deep v bicycle rights banh mi YOLO skateboard wayfarers lyft fashion axe. Schlitz chambray chartreuse, brooklyn vape viral fam occupy. VHS bitters kickstarter single-origin coffee fingerstache man braid keytar four loko flexitarian. Vinyl twee hot chicken, hella snackwave humblebrag cardigan kale chips tumeric health goth. Occupy ramps next level gastropub echo park chicharrones lo-fi activated charcoal lomo pork belly kitsch gentrify viral. Letterpress health goth street art, pabst green juice portland tilde direct trade small batch stumptown.</p>
+					<p>Cardigan poke organic pinterest gentrify selfies. Gentrify jianbing cardigan food truck, asymmetrical salvia chartreuse bitters. Hexagon next level locavore retro snackwave neutra brooklyn. Aesthetic flexitarian edison bulb subway tile paleo, pabst XOXO. Flannel art party neutra, vegan roof party pour-over gastropub la croix kinfolk kitsch cold-pressed aesthetic cardigan XOXO. Pickled thundercats skateboard deep v intelligentsia 8-bit. Beard 90's humblebrag shoreditch bicycle rights before they sold out heirloom.</p>
+					<p>Meggings live-edge la croix salvia shaman hammock neutra PBR&B locavore gastropub. Jianbing chillwave bitters sartorial. Thundercats fanny pack messenger bag, helvetica banjo twee pinterest post-ironic iPhone prism hell of. Tbh cold-pressed farm-to-table hammock thundercats direct trade, four dollar toast pok pok snackwave freegan roof party humblebrag PBR&B hot chicken truffaut. Knausgaard ethical gluten-free messenger bag deep v poutine. Vexillologist scenester PBR&B polaroid, slow-carb green juice YOLO humblebrag.</p>
+						
+					<br>
+					Oh. You need a little dummy text for your mockup? How quaint.
+					<br>
+					I bet you’re still using Bootstrap too…
+				</div>
+				<div class="imgs" style="background-color: #273859; padding: 3%; margin-bottom: 10%;"><h1 style="color: #5B85D9;">Everything from Strands to Twists</h1></div>
+				<div class="imgs" id="container" style="text-align: center;">
+					<div class="socialMedia">
+						<div style="width: 100%; background-color: #273859; height: 50px; color: #F2E7B4; font-size: 20px;">
+							Instagram
+						</div>
+					</div>
+					<div class="socialMedia"> YouTube</div>
+				</div>
+				<div class="imgs" style="text-align: left; padding: 10%;">
+					<p>Lorem ipsum dolor amet pitchfork occupy gluten-free, pour-over jean shorts shoreditch blue bottle echo park. Vaporware retro wayfarers, copper mug health goth tofu mixtape. Umami hell of humblebrag, jianbing raw denim tumblr you probably haven't heard of them meh ennui post-ironic jean shorts heirloom. Snackwave copper mug slow-carb adaptogen, tacos actually messenger bag pork belly craft beer sriracha food truck microdosing helvetica twee. Skateboard irony chambray prism live-edge pabst selvage post-ironic fam. Plaid scenester brunch single-origin coffee.</p>
+					<p>Roof party VHS tumeric trust fund, dreamcatcher shoreditch coloring book pok pok meh chicharrones leggings ethical etsy 3 wolf moon. Leggings synth af paleo 8-bit sustainable man braid bicycle rights meggings. Tote bag vegan yr put a bird on it four dollar toast lo-fi blue bottle wayfarers glossier trust fund small batch microdosing raclette edison bulb succulents. Humblebrag salvia blog seitan, pour-over raclette wolf paleo.</p>
+					<p>Adaptogen portland deep v bicycle rights banh mi YOLO skateboard wayfarers lyft fashion axe. Schlitz chambray chartreuse, brooklyn vape viral fam occupy. VHS bitters kickstarter single-origin coffee fingerstache man braid keytar four loko flexitarian. Vinyl twee hot chicken, hella snackwave humblebrag cardigan kale chips tumeric health goth. Occupy ramps next level gastropub echo park chicharrones lo-fi activated charcoal lomo pork belly kitsch gentrify viral. Letterpress health goth street art, pabst green juice portland tilde direct trade small batch stumptown.</p>
+					<p>Cardigan poke organic pinterest gentrify selfies. Gentrify jianbing cardigan food truck, asymmetrical salvia chartreuse bitters. Hexagon next level locavore retro snackwave neutra brooklyn. Aesthetic flexitarian edison bulb subway tile paleo, pabst XOXO. Flannel art party neutra, vegan roof party pour-over gastropub la croix kinfolk kitsch cold-pressed aesthetic cardigan XOXO. Pickled thundercats skateboard deep v intelligentsia 8-bit. Beard 90's humblebrag shoreditch bicycle rights before they sold out heirloom.</p>
+					<p>Meggings live-edge la croix salvia shaman hammock neutra PBR&B locavore gastropub. Jianbing chillwave bitters sartorial. Thundercats fanny pack messenger bag, helvetica banjo twee pinterest post-ironic iPhone prism hell of. Tbh cold-pressed farm-to-table hammock thundercats direct trade, four dollar toast pok pok snackwave freegan roof party humblebrag PBR&B hot chicken truffaut. Knausgaard ethical gluten-free messenger bag deep v poutine. Vexillologist scenester PBR&B polaroid, slow-carb green juice YOLO humblebrag.</p>
+					
+					<br>
+					Oh. You need a little dummy text for your mockup? How quaint.
+					<br>
+					I bet you’re still using Bootstrap too…
+				</div>
+			</div>
+
+			<div id="footer">
+				© 2018 Trinity Sazo
+			</div>
+
+		</div>
+	</body>
+</html>
